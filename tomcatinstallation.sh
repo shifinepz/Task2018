405b114995ef603ac2edf46a0305b33251adbd26
shifin rafeeque <shifin.epz@gmail.com>
2018-11-28 18:57:53 +0530

    Tomcat installation

---
+++ tomcatinstallation.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo yum install tomcat -y
+sudo sed -i 's/port="8080"/port="80"/' /etc/tomcat/server.xml
+sudo yum install tomcat-webapps tomcat-admin-webapps -y
+sudo yum install tomcat-docs-webapp tomcat-javadoc -y
